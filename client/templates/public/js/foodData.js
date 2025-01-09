378210ef91615d8a110d23866bb21b8ac8d7c113
Kevin Guzman <kevincastrog28@gmail.com>
2025-01-09 11:34:48 -0500

    Logging API calls to console

--- client/templates/public/js/foodData.js
+++ client/templates/public/js/foodData.js
@@ -1,14 +1,33 @@
-const axios = require('axios')
+const axios = require('axios');
 require('dotenv').config();
 
-
-const API_KEY = process.env.APP_ID
-const APP_ID = process.env.API_KEY
+const API_KEY = process.env.API_KEY;
+const APP_ID = process.env.APP_ID;
 
 const callApi = () => {
-    let url = `https://api.edamam.com/api/recipes/v2&type=public&app_id=${APP_ID}&app_key=${API_KEY}`;
+    const url = `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`;
 
-
-}
+    axios.get(url)
+        .then((response) => {
+            // Check if there are hits
+            if (response.data.hits && response.data.hits.length > 0) {
+                // Iterate over each hit and log the recipe details
+                response.data.hits.forEach(hit => {
+                    const recipe = hit.recipe;
+                    console.log(`Recipe: ${recipe.label}`);
+                    console.log(`Source: ${recipe.source}`);
+                    console.log(`URL: ${recipe.url}`);
+                    console.log(`Image: ${recipe.image}`);
+                    console.log(`Ingredients: ${recipe.ingredientLines.join(', ')}`);
+                    console.log('-----------------------------------');
+                });
+            } else {
+                console.log('No recipes found.');
+            }
+        })
+        .catch((error) => {
+            console.error('Error fetching data:', error);
+        });
+};
 
 module.exports = callApi;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const apiRoutes = require("./api");
+const callApi = require('../../client/templates/public/js/foodData')
 
 // Home page
 router.get("/", (req, res) => {
@@ -14,7 +15,13 @@ router.get("/about", (req, res) => {
 
 // Chart/app page
 router.get("/chart", (req, res) => {
-  res.render("chart");
+  let responseData = callApi();
+
+  console.log(responseData)
+
+  res.render("chart", {
+    food: responseData
+  });
 });
 
 //login page
