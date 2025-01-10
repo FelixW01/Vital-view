c882b02d794260147b390cbd093355dddf90e5f2
Felix W <felixwillem01@yahoo.com>
2025-01-09 22:53:43 -0500

    rewrote model logic

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,10 +3,14 @@ const router = express.Router();
 const { registerUser, getUsers, loginUser, getMe } = require('../controllers/userController');
 const auth = require('../middleware/auth');
 
-// Register route for user
+// API routes for user
 router.get('/users', getUsers);
 router.post('/login', loginUser);
 router.post('/register', registerUser);
 router.get('/me', auth, getMe);
+
+// API routes for sugar
+
+// API for recipes
 
 module.exports = router;
